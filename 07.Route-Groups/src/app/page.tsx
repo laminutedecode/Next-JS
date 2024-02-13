cb597a11d389ab35b6ef51c893bdfd3bf76e9c76
Jbtzcodes <laminutedecode@gmail.com>
2024-02-13 02:51:32 +0100

    mise a jour

--- 07.Route-Groups/src/app/page.tsx
+++ 07.Route-Groups/src/app/page.tsx
@@ -1,4 +1,4 @@
-// Vous pouvez utiliser une convention de nommage pour indiquer qu'un dossier ou un fichier est privé. Par exemple, vous pourriez préfixer le nom du dossier ou du fichier avec un caractère de soulignement "_".
+// Les groupes de routes (ou route groups) dans Next.js sont une fonctionnalité qui vous permet d'organiser et de structurer vos routes de manière logique et hiérarchique. Cela vous permet de regrouper des routes liées entre elles sous un même préfixe d'URL, ce qui rend la gestion des routes plus efficace et plus organisée. Créer register,login et forgot-password dans un dossier auth
 
 export default function Home(){
   return (
@@ -7,3 +7,5 @@ export default function Home(){
     </div>
   )
 }
+
+//  Les groupes de routes vous permettent d'organiser vos routes de manière logique et hiérarchique. Cela rend la structure de votre application plus claire et plus facile à comprendre, car les routes liées entre elles sont regroupées sous un même préfixe d'URL.En regroupant des routes similaires sous un même groupe, vous simplifiez la gestion et la maintenance de votre application. De plus, les groupes de routes vous permettent de réutiliser des fonctionnalités ou des composants spécifiques à travers plusieurs routes et de plus elle sont tres flexible  dans la gestion des transitions de route et d'autres événements associés.  Vous pouvez associer des événements spécifiques à un groupe de routes pour exécuter une logique particulière lorsqu'une route du groupe est activée ou lorsqu'il y a un changement de route à l'intérieur du groupe. On peut acceder avec /auth/login etc.. mais si on mest le dossier entre () (auth) on pourra donc les regrouper et donc acceder a lurl /login etc...
